d9987d2bdd709d6366c5ba598fbe078bfcc53b7d
Tristan <tristanshawnjohnson@gmail.com>
2021-09-17 14:04:45 -0500

    Used my Vue-Express-Mongoose boilerplate and changed the name of database

--- server/database/index.js
+++ server/database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const yourDB = 'yourDatabaseName';
+const yourDB = 'Clients';
 const mongoURI = `mongodb://localhost/${yourDB}`;
 
 const db = mongoose.connect(mongoURI, (err, success) => {
