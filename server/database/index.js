7fca5c78ef1a1386a2d48f526fae7d7968910d74
Tristan <tristanshawnjohnson@gmail.com>
2021-09-18 12:11:15 -0500

    Refactored db to use promises

--- server/database/index.js
+++ server/database/index.js
@@ -2,14 +2,8 @@ const mongoose = require('mongoose');
 const yourDB = 'Clients';
 const mongoURI = `mongodb://localhost/${yourDB}`;
 
-const db = mongoose.connect(mongoURI, (err, success) => {
-
-  if (err) {
-    console.log(err)
-  } else {
-    console.log('Connected to MongoDB')
-  }
-
-})
+const db = mongoose.connect(mongoURI)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.log('Connection error ', err));
 
 module.exports = db;
