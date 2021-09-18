4f400066a846ff1989ce5811dd07cb0caeca537d
Tristan <tristanshawnjohnson@gmail.com>
2021-09-18 12:24:31 -0500

    Created models for both Client and Provider. I decided to let MongoDB create the indexes. They are guaranteed unique and why do something it does for you?

--- server/database/dbInstructions.js
+++ server/database/dbInstructions.js
@@ -1,4 +1,31 @@
 const mongoose = require('mongoose');
 const db = require('./index.js')
 
-//Database Instructions Go Here
+//Create Client and Provider collections and populate them with dummy documents
+
+const Client = mongoose.model(
+  "Client",
+  new mongoose.Schema({
+    name: String,
+    email: String,
+    phone: Number,
+    providers: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Provider"
+    }]
+  })
+)
+
+const Provider = mongoose.model(
+  "Provider",
+  new mongoose.Schema({
+    name: String,
+    clients: [
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Client"
+    ]
+  })
+)
+
+
+
