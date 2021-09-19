9837258b8994d461f0b52c7d513ba8307302c380
Tristan <tristanshawnjohnson@gmail.com>
2021-09-19 10:55:38 -0500

    Implemented an addMultipleProviders model

--- server/models/clientAndProvider.js
+++ server/models/clientAndProvider.js
@@ -5,16 +5,14 @@ const { Client, Provider } = require('../database/dbModels.js')
 
 const linkClientWithProvider = async function (clientId, providerId) {
 
-  let promise1 = Provider.findByIdAndUpdate(providerId,
-    {$push: {clients: clientId}}, {new: true})
+  let promise1 = Provider.findByIdAndUpdate(providerId, {$push: {clients: clientId}}, {new: true})
   .then(doc => {
     console.log(`Client ${clientId} added to provider ${providerId}`);
     return doc;
   })
   .catch(err => console.log(`Client ${clientId} was NOT added to provider ${providerId}`, err))
 
-  let promise2 = Client.findByIdAndUpdate(clientId,
-    {$push: {providers: providerId}}, {new: true})
+  let promise2 = Client.findByIdAndUpdate(clientId, {$push: {providers: providerId}}, {new: true})
   .then(doc => {
     console.log(`Provider ${providerId} added to client ${clientId} `);
     return doc;
@@ -29,3 +27,33 @@ const linkClientWithProvider = async function (clientId, providerId) {
   .catch(err => console.log('Something went wrong', err))
 
 }
+
+// Add multiple providers to single Client
+
+const addMultipleProviders = async function (clientId, arrayOfProvidersIds) {
+  // need a promise that adds all the providers in the array to the client array
+
+  const asyncLoop = async function () {
+    for (let i = 0; i < arrayOfProvidersIds.length; i++) {
+      await linkClientWithProvider(clientId, arrayOfProvidersIds[i])
+    }
+  }
+  asyncLoop();
+}
+//Test1 and all providers
+addMultipleProviders("61474c173acc1d07c8a49634", [
+  "61474c173acc1d07c8a4963a",
+  "61474c173acc1d07c8a49639",
+  "61474c173acc1d07c8a49638",
+  "61474c173acc1d07c8a49637",
+  "61474c173acc1d07c8a49636"
+])
+
+// Remove client from provider
+
+// Delete provider entirely
+
+// Get data
+
+// Get Clients Populated
+
