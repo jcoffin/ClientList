db09caa55501259878a4b1680d3683d84c1c5a3f
Tristan <tristanshawnjohnson@gmail.com>
2021-09-19 15:30:45 -0500

    Implemented getAllClients not populated

--- server/models/clients.js
+++ server/models/clients.js
@@ -3,6 +3,20 @@ const { Client } = require('../database/dbModels.js')
 
 // Get all Clients
 
+const getAllClients = async function () {
+  let clients = await Client.find({})
+  .lean()
+  .then(docs => {
+    console.log('Successfully read all clients');
+    return docs;
+  })
+  .catch(err => console.log('Could not read all clients', err))
+
+  return clients;
+}
+
+getAllClients()
+
 // Get single Client by Id
 
 // Get single Client Id by name
