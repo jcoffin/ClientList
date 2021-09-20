c527bfc27b1b82faf2f19ad1e839c5aeb83b6ba9
Tristan <tristanshawnjohnson@gmail.com>
2021-09-20 10:43:39 -0500

    Refactored linkClientToProvider and renamed it addProviderToClient

--- server/models/clientAndProvider.js
+++ server/models/clientAndProvider.js
@@ -3,30 +3,18 @@ const { Client, Provider } = require('../database/dbModels.js')
 
 // This adds a clientId to a provider's provider array and adds the corresponding providerId to that client's provider array
 
-const linkClientWithProvider = async function (clientId, providerId) {
+const addProviderToClient = async function (clientId, providerId) {
 
-  let promise1 = Provider.findByIdAndUpdate(providerId, {$push: {clients: clientId}}, {new: true})
-  .then(doc => {
-    console.log(`Client ${clientId} added to provider ${providerId}`);
-    return doc;
-  })
-  .catch(err => console.log(`Client ${clientId} was NOT added to provider ${providerId}`, err))
-
-  let promise2 = Client.findByIdAndUpdate(clientId, {$push: {providers: providerId}}, {new: true})
+  Client.findByIdAndUpdate(clientId, {$push: {providers: providerId}}, {new: true})
   .then(doc => {
     console.log(`Provider ${providerId} added to client ${clientId} `);
     return doc;
   })
   .catch(err => console.log(`Provider ${providerId} was NOT added to client ${clientId} `, err))
 
-  Promise.all([promise1, promise2])
-  .then(values => {
-    console.log('Update was sucessful', values[0], values[1]);
-    return values;
-  })
-  .catch(err => console.log('Something went wrong', err))
-
 }
+
+addProviderToClient("6148a07a298152bb3cf48c14", "6148a37489fd79eebfd2b6f8")
 
 // Add multiple providersIds to single Client's providers array and that clientId to all provider's client array that are in the second argument
 
@@ -191,7 +179,7 @@ deleteClient("61474c173acc1d07c8a49633")
 
 
 module.exports = {
-  linkClientWithProvider,
+  addProviderToClient,
   addMultipleProviders,
   removeProvider,
   removeMultipleProviders,
